b4695245f606295edb460088faf0405554bb7a2c
Priyam Gupta <priyamgupta@myworkforce.org>
2018-07-26 01:04:11 +0530

    price chart

--- src/app/price-chart/price-chart.component.ts
+++ src/app/price-chart/price-chart.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ListcurrencyService } from '../listcurrency.service';
 import { Chart } from 'chart.js';
+// import {Observable} from 'rxjs';
 
 
 
@@ -11,90 +12,66 @@ import { Chart } from 'chart.js';
   styleUrls: ['./price-chart.component.css']
 })
 export class PriceChartComponent implements OnInit {
-  public selectedCoin =[];
+  public selectedCoin = [];
   public allCurrency = [];
   public arr = [];
   public arrCopy = [];
   chart = [];
-  priceDate =[];
+  timelines = [];
   date_1 = [];
-  constructor(private _route: ActivatedRoute, private router: Router,public listService: ListcurrencyService) { }
+  public TIME_INTERVAL = 3600 * 1000;
+  public TIMELINE_LENGTH = 12;
+  constructor(private _route: ActivatedRoute, private router: Router, public listService: ListcurrencyService) { }
 
   ngOnInit() {
-    let myCoinId = this._route.snapshot.paramMap.get('id');
-    this.listService.getAllCurrency().subscribe(
-      data => {
-        this.allCurrency = data.data;
+    this._init();
+  }
 
-        for (let element in this.allCurrency) {
-          this.arr.push(this.allCurrency[element]);
-        }
-        this.arrCopy = this.arr;
-        this.selectedCoin = this.arrCopy.filter(word => word.id == myCoinId);
-
-        let date = new Date(this.selectedCoin[0].last_updated*1000);
-        var hours = date.getHours();
-       
-       // console.log(price);
-     
-      for(let i=0;i<12;i++)
-      {
-        var priceChange = this.selectedCoin[0].quotes.USD.percent_change_1h; 
-        var price = this.selectedCoin[0].quotes.USD.price; 
-        if(priceChange <= 0)
-        {
-           price = price + price*(priceChange/100);
-        }
-        else{
-          price = price - price*(priceChange/100);
-        }
-        this.priceDate.push({  
-        "hourGraph" : hours,
-        "priceGraph" : price
-        });
-        if(i === 12)
-        {
-          this.priceDate.shift();
-        }
-      
-      }
-   console.log(this.priceDate);
-        this.chart = new Chart('canvas',{
-          type: 'line',
-          data: {
-            labels: [this.priceDate[0].hourGraph],
-            dataset : [{
-              data: [this.priceDate[0].priceGraph]
-            }]
-          },
-          options : {
-            legend : {
-              display: false
-            },
-            scales : {
-              xAxes : [{
-                display: true
-              }],
-              yAxes : [{
-                display: true
-              }]
-            }
+  _init()
+  {
+    setInterval(() => {
+      let myCoinId = this._route.snapshot.paramMap.get('id');
+      this.listService.getAllCurrency().subscribe(
+        data => {
+          this.allCurrency = data.data;
+  
+          for (let element in this.allCurrency) {
+            this.arr.push(this.allCurrency[element]);
           }
-        })
-      
-    },
-      error => {
-        console.log(error);
-      }
-    
-    )
+          this.arrCopy = this.arr;
+          this.selectedCoin = this.arrCopy.filter(word => word.id == myCoinId);
+          let date = new Date(this.selectedCoin[0].last_updated * 1000);
+            var hours = date.getHours();
   
-   }
-
+            var priceChange = this.selectedCoin[0].quotes.USD.percent_change_1h;
+            var price = this.selectedCoin[0].quotes.USD.price;
+            if (priceChange <= 0) {
+              price = price + price * (priceChange / 100);
+            }
+            else {
+              price = price - price * (priceChange / 100);
+            }
+            this.timelines.push({
+              "hourGraph": hours,
+              "priceGraph": price
+            });
+            if (this.timelines.length === this.TIMELINE_LENGTH) {
+              this.timelines.shift();
+            }
+          }, this.TIME_INTERVAL);
+        },
   
-
-   
-
+        error => {
+          console.log(error);
+        }
   
- 
+      )
+
+  // console.log(this.timelines);
+  }
+
+
+
+
+
 }
